5b3d4712e5670405ba932ad80f751f3eba42e989
sachiko-kame <sachiko-kame@users.noreply.github.com>
2019-09-04 04:01:58 +0900

    キャッシュするまで

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.routing.registerRoute(
+    '/',
+    new workbox.strategies.NetworkFirst({
+        cacheName: 'cache',
+    })
+);
+
+workbox.routing.registerRoute(
+    new RegExp(/.*\.js/),
+    new workbox.strategies.NetworkFirst({
+        cacheName: 'js-cache',
+    })
+);
